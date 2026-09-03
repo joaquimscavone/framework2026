dbbd01440b362eee279a5337861d05218f002123
Prof. Joaquim Scavone <joaquim.scavone@ifto.edu.br>
2026-09-03 09:40:43 -0300

    Route Finalizada

--- framework/Facades/Route.php
+++ framework/Facades/Route.php
@@ -2,12 +2,14 @@
 
 namespace Fmk\Facades;
 
+use Exception;
 use Fmk\Enums\Methods;
 /**
  * Importações
  * use classe requerida;
  */
-class Route{
+class Route
+{
     protected string $uri;
     protected $callback;
     protected array $paramns;
@@ -15,45 +17,73 @@ class Route{
     protected string $name;
     protected bool $active;
 
-    public function __construct(string $name, string $uri, Methods $method,callable|array $callback){
+    public function __construct(string $name, string $uri, Methods $method, callable|array $callback)
+    {
         $this->name = $name;
         $this->uri = $uri;
         $this->method = $method;
         $this->callback = $callback;
         $this->active = false;
-        $this->paramns = array_fill_keys($this->prepareParamns($uri),null);
+        $this->paramns = array_fill_keys($this->prepareParamns($uri), null);
     }
 
 
-    protected function prepareParamns($uri){
-        //professor/{id_professor}/disciplinas/{id_disciplina}/aulas/{id_aula}
+    protected function prepareParamns($uri)
+    {
         //id_diciplina 
         //disciplinas/{id_disciplina}/swap/{professor_1}/{professor_2}
         //exemplo: ((protected|private|public) [a-z]{1,} \$)
         $exp = "(\{[a-z0-9_\}]{1,})";
-        if(preg_match_all($exp,$uri,$matches)){
-            return preg_replace('(\{|\})','',$matches[0]);
+        if (preg_match_all($exp, $uri, $matches)) {
+            return preg_replace('(\{|\})', '', $matches[0]);
         }
         return [];
     }
 
-    public function setParamns($paramns){
-        foreach($paramns as $key => $param ){
-            if(array_key_exists($key, $this->paramns)){
+    public function setParamns($paramns)
+    {
+        foreach ($paramns as $key => $param) {
+            if (array_key_exists($key, $this->paramns)) {
                 $this->paramns[$key] = $param;
             }
         }
     }
 
-    public function defineParamns($paramns){
-        foreach($this->paramns as &$param){
+    public function defineParamns($paramns)
+    {
+        foreach ($this->paramns as &$param) {
             $param = array_shift($paramns);
         }
     }
 
-    public function getParamns(){
+    public function getParamns()
+    {
         return $this->paramns;
     }
+
+    public function getUrl()
+    {
+        $url = $this->uri;
+        foreach ($this->paramns as $key => $value) {
+            if (is_null($value)) {
+                throw new Exception("$key não pode ser null para essa URL");
+            }
+            $url = str_replace("{" . $key . "}", $value, $url);
+
+        }
+        return $url;
+    }
+
+    public function exec(){
+       $callback = $this->callback;
+       if(is_array($callback) && class_exists($callback[0])){
+            $callback[0] = new $callback[0];
+       }
+       $this->active = true;
+       return call_user_func_array($callback, $this->paramns);
+    }
+
+
 
 
 
--- public/index.php
+++ public/index.php
@@ -3,36 +3,5 @@
 
 require_once __DIR__."/../app/application.php";
 
-use App\Controllers\ListarUsuariosController;
 use Fmk\Enums\Methods;
 use Fmk\Facades\Route;
-
-// var_dump($_SERVER['REQUEST_METHOD']);
-// $controller = new App\Controllers\ListarUsuariosController();
-// $controller->caju();
-//usuario/5
-// new Route('usuario.view','professor/',Methods::GET,function(){});
-$route = new Route('usuario.view','professor/{id_professor}/disciplinas/{id_disciplina}/aulas/{id_aula}',Methods::GET,function(){});
-
-
-$route->defineParamns(['id_professor' => 15, 'id_diciplina'=>23, 'id_aula'=>45]);
-
-
-echo '<pre>';
-var_dump($route->getParamns());
-
-
-
-
-
-
-// use App\Controllers\ListarUsuariosController;
-// $controller = new ListarUsuariosController();
-// $rota = $_GET['rota'] ?? 'listar';
-
-// if(method_exists($controller,$rota)){
-//     $controller->$rota();
-// }else{
-//     http_response_code(404);
-//     die('404 - NOT FOUND!');
-// }
